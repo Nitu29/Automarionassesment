73e8caeb32b2f2cbfe3e15889e071301e3a4ed8e
Nitu29 <nitupanjiyar@gmail.com>
2021-08-30 14:47:03 +0530

    Added Test cases to verify address Text field

--- src/test/java/com/midtrans/Testcases/Testcasesformidtran.java
+++ src/test/java/com/midtrans/Testcases/Testcasesformidtran.java
@@ -70,4 +70,16 @@ public class Testcasesformidtran extends Baseclass{
         Assert.assertEquals(S1,"Jakarta");
         System.out.println("Assert is passed");
     }
+    @Test(priority = 6)
+    public void verifyaddresstextfield()
+    {
+        Checkoutpage checkoutpage= new Checkoutpage(driver);
+        //Homepage homepage= new Homepage(driver);
+        //homepage.clickonBuynowbutton();
+        Baseclass.holdExecutionForSeconds(2);
+        String S1=checkoutpage.getthevalueenteredinAddressfield();
+        System.out.println(S1);
+        Assert.assertEquals(S1,"MidPlaza 2, 4th Floor Jl.Jend.Sudirman Kav.10-11");
+        System.out.println("Assert is passed");
+    }
 }
